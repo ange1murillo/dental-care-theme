10676ba0c8b2b10487d64072906df622579e13b2
ange1murillo <>
2024-01-09 10:16:04 -0600

    removed show hide func

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -7,26 +7,26 @@ btn.addEventListener("click", () => {
   nav.classList.toggle("hidden");
 });
 
-let lastScrollPosition = 0;
+// let lastScrollPosition = 0;
 
-window.addEventListener("scroll", () => {
-  const currentScrollPosition = window.scrollY;
+// window.addEventListener("scroll", () => {
+//   const currentScrollPosition = window.scrollY;
 
-  if (currentScrollPosition > lastScrollPosition) {
-    // Scrolling down
-    // document.getElementById("secondaryNav").style.transform =
-    //   "translateY(-100%)";
+//   if (currentScrollPosition > lastScrollPosition) {
+//     // Scrolling down
+//     // document.getElementById("secondaryNav").style.transform =
+//     //   "translateY(-100%)";
 
-    document.getElementById("mainNav").style.transform = "translateY(-100%)";
-  } else {
-    // Scrolling up
-    // document.getElementById("secondaryNav").style.transform =
-    //   "translateY(0)";
-    document.getElementById("mainNav").style.transform = "translateY(0)";
-  }
+//     document.getElementById("mainNav").style.transform = "translateY(-100%)";
+//   } else {
+//     // Scrolling up
+//     // document.getElementById("secondaryNav").style.transform =
+//     //   "translateY(0)";
+//     document.getElementById("mainNav").style.transform = "translateY(0)";
+//   }
 
-  lastScrollPosition = currentScrollPosition;
-});
+//   lastScrollPosition = currentScrollPosition;
+// });
 
 // document.addEventListener("DOMContentLoaded", function () {
 //   var hero = document.getElementById("hero");
@@ -43,16 +43,16 @@ window.addEventListener("scroll", () => {
 //   });
 // });
 
-document.addEventListener("DOMContentLoaded", function () {
-  var hero = document.getElementById("hero");
+// document.addEventListener("DOMContentLoaded", function () {
+//   var hero = document.getElementById("hero");
 
-  window.addEventListener("scroll", function () {
-    // Check if the user has scrolled down
-    if (window.scrollY > 0) {
-      hero.style.marginTop = "0px";
-    } else {
-      // Reset to the initial margin when the user scrolls to the top
-      hero.style.marginTop = "80px";
-    }
-  });
-});
+//   window.addEventListener("scroll", function () {
+//     // Check if the user has scrolled down
+//     if (window.scrollY > 0) {
+//       hero.style.marginTop = "0px";
+//     } else {
+//       // Reset to the initial margin when the user scrolls to the top
+//       hero.style.marginTop = "80px";
+//     }
+//   });
+// });
